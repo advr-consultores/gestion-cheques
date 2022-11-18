b294e788452ff17ae6ce413e4fab2a89fde1caf5
nandoferth <vizcainolopezfernando1996@outlook.es>
2022-11-17 22:23:29 -0600

    add env to firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -6,17 +6,16 @@ import { getAuth } from 'firebase/auth'
 import { getMessaging } from "firebase/messaging/sw";
 import { initializeApp } from "firebase/app";
 
-
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyDNTExAc8-cRl1aBzJokUwjW5lW534sovY",
-  authDomain: "chequesadvr.firebaseapp.com",
-  databaseURL: "https://chequesadvr-default-rtdb.firebaseio.com",
-  projectId: "chequesadvr",
-  storageBucket: "chequesadvr.appspot.com",
-  messagingSenderId: "825162486096",
-  appId: "1:825162486096:web:46ae51a99cb5ccc6e31c55",
-  measurementId: "G-0S7RENS99H"
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_DATABASE_URL,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_APP_ID,
+  measurementId: process.env.VUE_APP_MEASUREMENT_ID
 }
 
 const app = initializeApp(firebaseConfig);
